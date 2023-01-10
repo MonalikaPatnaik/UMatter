b6bd7367687cd843881c625209890730ecc08330
Monalika Patnaik <monalikapatnaik114@gmail.com>
2023-01-10 22:15:14 +0530

    chatbot added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import SigninPage from './pages/signin';
-
+// window.$crisp=[];
+// window.CRISP_WEBSITE_ID="e79efdd2-abee-4a1e-b868-c7929585ebd9";
 
 function App() {
   return (
