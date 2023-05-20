b6b208e64c7bad0eace6a9a3a6fa0439321f477e
Prajwal Jayant Somalkar <prajwalsomalkar025@gmail.com>
2023-05-20 13:06:29 +0530

    removing underline form Get Started Button

--- src/Components/InfoSection/index.js
+++ src/Components/InfoSection/index.js
@@ -1,39 +1,39 @@
 import React from 'react'
-import {Button} from '../ButtonElements'
-import { AboutContainer, AboutWrapper, AboutRow,Column1, Column2, TextWrapper, TopLine, Heading, SubTitle, BtnWrap , ImgWrap, Img } from './InfoElements'
+import { Button } from '../ButtonElements'
+import { AboutContainer, AboutWrapper, AboutRow, Column1, Column2, TextWrapper, TopLine, Heading, SubTitle, BtnWrap, ImgWrap, Img } from './InfoElements'
 
-const About = ({lightBg, id, imgStart, topLine, headline, description, buttonLabel, lightText, darkText, img, alt, primary, dark, dark2 }) => {
+const About = ({ lightBg, id, imgStart, topLine, headline, description, buttonLabel, lightText, darkText, img, alt, primary, dark, dark2 }) => {
   return (
     <>
       <AboutContainer lightBg={lightBg} id={id}>
         <AboutWrapper>
-            <AboutRow imgStart={imgStart}>
-                <Column1>
-                <TextWrapper>
-                    <TopLine>{topLine}</TopLine>
-                    <Heading lightText = {lightText}>{headline}</Heading>
-                    <SubTitle darkText={darkText}>{description}</SubTitle>
-                    <BtnWrap>
-                        <Button to='home'
-                        smooth = {true}
-                        duration = {500}
-                        spy={true}
-                        exact="true"
-                        offset={-80}
-                        primary={primary ? 1 : 0}
-                        dark={dark ? 1 : 0}
-                        dark2={dark2 ? 1: 0}
-                        
-                        >{buttonLabel} </Button>
-                    </BtnWrap>
-                </TextWrapper>
-                </Column1>
-                <Column2>
-                <ImgWrap>
-                <Img src={img} alt={alt}/> 
-                </ImgWrap>
-                </Column2>
-            </AboutRow>
+          <AboutRow imgStart={imgStart}>
+            <Column1>
+              <TextWrapper>
+                <TopLine>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headline}</Heading>
+                <SubTitle darkText={darkText}>{description}</SubTitle>
+                <BtnWrap>
+                  <Button to='home'
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact="true"
+                    offset={-80}
+                    primary={primary ? 1 : 0}
+                    dark={dark ? 1 : 0}
+                    dark2={dark2 ? 1 : 0}
+                    style={{ textDecoration: 'none' }}
+                  >{buttonLabel} </Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={img} alt={alt} />
+              </ImgWrap>
+            </Column2>
+          </AboutRow>
         </AboutWrapper>
 
       </AboutContainer>
