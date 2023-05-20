c43ab501bf1c7bcac76b218a85726d563dc51946
Prashant-2505 <shramaprashant2505@gmail.com>
2023-05-20 20:40:09 +0530

    solved the sign up issue
    add google authentication by using firebase
    
    add some other functionality like when user logged in ther it will show
    profile instead of sign in
    
    adding sign out functionlity
    
    when your signed in then it will redirect user to home page
    
    when your signed out then it will redirect user to home page

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,37 +1,52 @@
-import React from 'react'
-import {FaBars} from 'react-icons/fa'
-import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, Navitem, NavLinks,NavBtn, NavBtnLink} from './NavbarElements';
+import React, { useEffect, useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, Navitem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({toggle}) => {
+const Navbar = ({ toggle }) => {
+  const auth = getAuth();
+  const [pagestate, setPageState] = useState('/LoginPage');
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState('Profile');
+      } else {
+        setPageState('Sign In');
+      }
+    });
+  }, [auth]);
+
   return (
     <>
       <Nav>
         <NavbarContainer>
-            <NavLogo to='/'>UMatter</NavLogo>
-            <MobileIcon onClick={toggle}>
-                <FaBars/>
-            </MobileIcon>
-            <NavMenu>
-                <Navitem>
-                    <NavLinks to='home'>Home</NavLinks>
-                </Navitem>
-                <Navitem>
-                    <NavLinks to='about'>About</NavLinks>
-                </Navitem>
-                <Navitem>
-                    <NavLinks to='services'>Services</NavLinks>
-                </Navitem>
-                <Navitem>
-                    <NavLinks to='signup'>Sign Up</NavLinks>
-                </Navitem>
-            </NavMenu>
-            <NavBtn>
-                <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-            </NavBtn>
+          <NavLogo to='/'>UMatter</NavLogo>
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <Navitem>
+              <NavLinks to='/home'>Home</NavLinks>
+            </Navitem>
+            <Navitem>
+              <NavLinks to='/about'>About</NavLinks>
+            </Navitem>
+            <Navitem>
+              <NavLinks to='/services'>Services</NavLinks>
+            </Navitem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to={`${pagestate === 'Profile' ? '/profile' : '/signin'}`}>
+              {pagestate}
+            </NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
