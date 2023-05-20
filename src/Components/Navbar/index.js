08bf805ae2d733e8cd5b3dff63b409f8bbdd0466
Piyush Arora <74443858+Piyusharora2003@users.noreply.github.com>
2023-05-20 21:27:36 +0530

    navbar updated
    #86 more responsive , #96

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,37 +1,71 @@
-import React from 'react'
-import {FaBars} from 'react-icons/fa'
-import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, Navitem, NavLinks,NavBtn, NavBtnLink} from './NavbarElements';
+import React, { useEffect, useState } from 'react';
+import styles from "./nav.module.css"
+import { HashLink as Linked } from 'react-router-hash-link';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({toggle}) => {
-  return (
-    <>
-      <Nav>
-        <NavbarContainer>
-            <NavLogo to='/'>UMatter</NavLogo>
-            <MobileIcon onClick={toggle}>
-                <FaBars/>
-            </MobileIcon>
-            <NavMenu>
-                <Navitem>
-                    <NavLinks to='home'>Home</NavLinks>
-                </Navitem>
-                <Navitem>
-                    <NavLinks to='about'>About</NavLinks>
-                </Navitem>
-                <Navitem>
-                    <NavLinks to='services'>Services</NavLinks>
-                </Navitem>
-                <Navitem>
-                    <NavLinks to='signup'>Sign Up</NavLinks>
-                </Navitem>
-            </NavMenu>
-            <NavBtn>
-                <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-            </NavBtn>
-        </NavbarContainer>
-      </Nav>
-    </>
-  )
+const data = {
+  name:"UMatter",
 }
 
-export default Navbar
+function Navbar(){
+const [open,setstate] = useState(0);
+const toggleBodyClass = () => {
+  document.body.classList.toggle(styles.menuActive);
+  (open === 0 )? setstate(1) : setstate(0);
+  if(open === 1){
+        // something to set body height max 100vh
+        document.body.style.maxHeight = "100vh";
+        document.body.style.overflow = "hidden";
+      }else{
+        document.body.style.maxHeight = "max-content"; 
+        document.body.style.overflow = "auto";        
+      }
+    }
+    
+    useEffect(()=>{
+      if(open === 1){
+        // something to set body height max 100vh
+        document.body.style.maxHeight = "100vh";
+        document.body.style.overflow = "hidden";
+       }else{
+        document.body.style.maxHeight = "max-content"; 
+        document.body.style.overflow = "auto";        
+       }
+    },[toggleBodyClass])
+
+  return (
+      <nav className={styles.navbar}>
+        <div className={styles.name}>{data.name}</div>
+        <div className={styles.pushLeft}>
+          <button id="menu-toggler" data-class={styles.menuActive} className={styles.hamburger} onClick={toggleBodyClass}>
+            {
+                open === 0 ?
+                <i className="fa-solid fa-bars"></i>
+                :
+                <i className="fa-solid fa-xmark"></i>
+            }
+          </button>
+          <ul id="primary-menu" className={`${styles.menu} ${styles.navMenu}`}>
+            <li className={styles.menuItem}>
+              <Linked className={styles.navLink} href="#home">Home</Linked>
+            </li>
+            <li className={styles.menuItem}>
+            <Linked className={styles.navLink} smooth to="#about">About</Linked>
+            </li>
+            <li className={styles.menuItem}>
+            <Linked className={styles.navLink} smooth to="#services">Services</Linked>
+            </li>
+            <li className={styles.menuItem}>
+            <Linked className={styles.navLink} smooth to="#signup">Signup</Linked>
+            </li>
+            <li className={styles.menuItem}>
+              <Link className={styles.signinbtn} to={'/signin'}>Signin</Link>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+  );
+}
+
+export default Navbar;
