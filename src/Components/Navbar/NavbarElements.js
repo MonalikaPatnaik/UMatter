adadbd2869d73911bf4a2b9c111e8bd18c97ed41
Anupreet1213 <anupreet.2003@gmail.com>
2023-05-20 18:28:28 +0530

    Updated UI for navbar

--- src/Components/Navbar/NavbarElements.js
+++ src/Components/Navbar/NavbarElements.js
@@ -1,16 +1,11 @@
-import styled from 'styled-components';
-import {Link as LinkR} from 'react-router-dom'
-import {Link as LinkS} from 'react-scroll'
+import styled from "styled-components";
+import { Link as LinkR } from "react-router-dom";
+import { Link as LinkS } from "react-scroll";
 
 export const Nav = styled.nav`
-  background: #000;
-<<<<<<< Updated upstream
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
   height: 80px;
-  margin-top : -80px; 
-=======
-  height: 60px;
-  margin-top : -80px;
->>>>>>> Stashed changes
+  margin-top: -80px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -19,83 +14,123 @@ export const Nav = styled.nav`
   top: 0;
   z-index: 10;
 
-  @media screen and (max-width: 960px){
+  @media screen and (max-width: 960px) {
     transition: 0.8s all ease;
   }
-`
+`;
 
 export const NavbarContainer = styled.div`
   display: flex;
-  justify-content: space-between;
-  height: 80px;
+  justify-content: space-around;
+  align-items: center;
+  ${"" /* height: 80px; */}
   z-index: 1;
   width: 100%;
-  padding: 0 24px;
-  max-width: 1100px;
-`
+  ${"" /* padding: 0 24px; */}
+  ${"" /* max-width: 1100px; */}
+  @media screen and (max-width: 768px) {
+    justify-content: space-between;
+    padding-left: 4%;
+    padding-right: 4%;
+  }
+`;
 
 export const NavLogo = styled(LinkR)`
   color: #fff;
-  justify-self: flex-start;
+  ${"" /* justify-self: flex-start; */}
   cursor: pointer;
   font-size: 1.5rem;
   display: flex;
   align-items: center;
-  margin-left: 24px;
+  ${"" /* margin-left: 24px; */}
   font-weight: bold;
   text-decoration: none;
-`
+`;
 export const MobileIcon = styled.div`
   display: none;
 
-  @media screen and (max-width: 768px){
+  @media screen and (max-width: 768px) {
     display: block;
-    position: absolute;
+    ${
+      "" /* position: absolute;
     top: 0;
     right: 0;
-    transform: translate(-100%, 60%);
+    transform: translate(-100%, 60%); */
+    }
     font-size: 1.8rem;
     cursor: pointer;
     color: #fff;
+    align-items: center;
+    display: flex;
   }
-`
+`;
 export const NavMenu = styled.ul`
   display: flex;
-  align-items: center;
+  gap: 20px;
+  ${
+    "" /* align-items: center;
+  justify-content: center; */
+  }
   list-style: none;
-  text-align: center;
-  margin-right: -22px; 
+  ${"" /* text-align: center; */}
+  ${"" /* margin-right: -22px; */}
+  margin-bottom : 0;
 
-  @media screen and  (max-width: 768px){
+  @media screen and (max-width: 768px) {
     display: none;
   }
-
-`
+`;
 
 export const Navitem = styled.li`
-  height: 80px;
-`
+  ${"" /* height: 80px; */}
+`;
 export const NavLinks = styled(LinkS)`
   color: #fff;
   display: flex;
   align-items: center;
   text-decoration: none;
-  padding: 0 1rem;
-  height: 100%;
+  ${"" /* padding: 0 1rem; */}
+  ${"" /* height: 100%; */}
   cursor: pointer;
+  position: relative;
+
+  ${
+    "" /* &:hover {
+    color: red;
+  } */
+  }
+
+  &::after {
+    content: "";
+    width: 0%;
+    height: 5%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background-color: #28b86b;
+    transition: all 0.3s ease-in;
+  }
+
+  &:hover:after {
+    width: 100%;
+  }
+
+  &:hover {
+    color: #28b86b;
+  }
 
   &.active {
     border-bottom: 3px solid #01bf71;
   }
-`
+`;
 export const NavBtn = styled.nav`
   display: flex;
   align-items: center;
 
-  @media screen and  (max-width: 768px){
-    display: none;  
+  @media screen and (max-width: 768px) {
+    display: none;
   }
-`
+`;
 export const NavBtnLink = styled(LinkR)`
   border-radius: 50px;
   background: #01bf71;
@@ -114,4 +149,4 @@ export const NavBtnLink = styled(LinkR)`
     background: #fff;
     color: #010606;
   }
-`
+`;
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,37 +1,62 @@
-import React from 'react'
-import {FaBars} from 'react-icons/fa'
-import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, Navitem, NavLinks,NavBtn, NavBtnLink} from './NavbarElements';
+import React, { useEffect, useState } from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  Navitem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
 
-const Navbar = ({toggle}) => {
+const Navbar = ({ toggle }) => {
+  const [navbarBg, setNavbarBg] = useState("transparent");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrolled = window.scrollY;
+      setNavbarBg(scrolled > 0 ? "rgba(0, 0, 0, 0.9)" : "");
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <Nav>
+      <Nav style={{ background: navbarBg }}>
         <NavbarContainer>
-            <NavLogo to='/'>UMatter</NavLogo>
-            <MobileIcon onClick={toggle}>
-                <FaBars/>
-            </MobileIcon>
-            <NavMenu>
-                <Navitem>
-                    <NavLinks to='home'>Home</NavLinks>
-                </Navitem>
-                <Navitem>
-                    <NavLinks to='about'>About</NavLinks>
-                </Navitem>
-                <Navitem>
-                    <NavLinks to='services'>Services</NavLinks>
-                </Navitem>
-                <Navitem>
-                    <NavLinks to='signup'>Sign Up</NavLinks>
-                </Navitem>
-            </NavMenu>
-            <NavBtn>
-                <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-            </NavBtn>
+          <NavLogo to="/">UMatter</NavLogo>
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <Navitem>
+              <NavLinks to="home">Home</NavLinks>
+            </Navitem>
+            <Navitem>
+              <NavLinks to="about">About</NavLinks>
+            </Navitem>
+            <Navitem>
+              <NavLinks to="services">Services</NavLinks>
+            </Navitem>
+            <Navitem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </Navitem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
