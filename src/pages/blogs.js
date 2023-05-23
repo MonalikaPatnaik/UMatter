a930c100cad5995ed0f454f05721994843d98704
Aneesh <aneeshx000@gmail.com>
2023-05-23 11:19:15 +0530

    talks section added

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -2,12 +2,13 @@ import React from "react";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 import Blogs from "../Components/Blogs";
-
+import Reviews from "../Components/Blogs/review";
 const BlogPage = () => {
   return (
     <>
       <Navbar />
       <Blogs />
+      <Reviews />
       <Footer />
     </>
   );
