86f6e3fb9e5374cf1a0473a0304f134ca7980996
Prabhroop-Singh2002 <phoenixl23800@gmail.com>
2023-05-23 11:02:41 +0530

    Adjusted the position of eye icon.

--- src/Components/Signin/index.js
+++ src/Components/Signin/index.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from 'react';
+import React from "react";
 import {
   Container,
   Icons,
@@ -12,18 +12,18 @@ import {
   FormLabel,
   FormWrap,
   Text,
-} from './SigninElements';
-import { useState } from 'react';
-import {forgotPassword} from '../Forgot_password/Forgot_password';
-import { NavLink } from 'react-router-dom';
+} from "./SigninElements";
+import { useState } from "react";
+import { forgotPassword } from "../Forgot_password/Forgot_password";
+import { NavLink } from "react-router-dom";
 const SignIn = () => {
-  const [passwordType, setPasswordType] = useState('password');
+  const [passwordType, setPasswordType] = useState("password");
   const handleclick = (e) => {
     e.preventDefault();
-    if (passwordType === 'text') {
-      setPasswordType('password');
+    if (passwordType === "text") {
+      setPasswordType("password");
     } else {
-      setPasswordType('text');
+      setPasswordType("text");
     }
   };
   return (
@@ -37,31 +37,43 @@ const SignIn = () => {
               <FormLabel htmlFor="for">Email</FormLabel>
               <FormInput placeholder="email@example.com" type="email" require />
               <FormLabel htmlFor="for">Password</FormLabel>
-              <FormInput
-                placeholder="Must have at least 8 characters"
-                type={passwordType}
-                require
-              />
-              <button
-                onClick={handleclick}
-                style={{
-                  width: 'fit-content',
-                  position: 'absolute',
-                  right: '38rem',
-                  bottom: '17rem',
-                  background: 'transparent',
-                  border: 'none',
-                }}>
-                {passwordType === 'password' ? (
-                  <i class="fa-solid fa-eye-slash" id="eye"></i>
-                ) : (
-                  <i class="fa-solid fa-eye" id="eye"></i>
-                )}
-              </button>
+              <div>
+                <FormInput
+                  style={{ width: "100%" }}
+                  placeholder="Must have at least 8 characters"
+                  type={passwordType}
+                  require
+                />
+                <button
+                  onClick={handleclick}
+                  style={{
+                    width: "fit-content",
+                    position: "relative",
+                    left: "90%",
+                    bottom: "65%",
+                    background: "transparent",
+                    border: "none",
+                  }}
+                >
+                  {passwordType === "password" ? (
+                    <i class="fa-solid fa-eye-slash" id="eye"></i>
+                  ) : (
+                    <i class="fa-solid fa-eye" id="eye"></i>
+                  )}
+                </button>
+              </div>
               <FormButton type="submit">Continue</FormButton>
-              <NavLink to="/signin/forgotPassword" style={{
-                textAlign:'center',color:'white',marginTop:'10px',textDecoration:'none'
-              }}>Forgot Password ?</NavLink>
+              <NavLink
+                to="/signin/forgotPassword"
+                style={{
+                  textAlign: "center",
+                  color: "white",
+                  marginTop: "10px",
+                  textDecoration: "none",
+                }}
+              >
+                Forgot Password ?
+              </NavLink>
             </Form>
           </FormContent>
         </FormWrap>
