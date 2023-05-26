4409b80ccfec74a84ea33d9b03c3131b2dfca0f3
Dimple choudhary <96401557+Dimple-Choudhary@users.noreply.github.com>
2023-05-26 02:31:31 +0000

    Fixes: Animate text #220

--- src/Components/Herosection/index.js
+++ src/Components/Herosection/index.js
@@ -1,36 +1,64 @@
-import React, { useState } from 'react'
-import Video from '../../videos/video1.mp4'
-import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroBtnWrapper, HeroH1, HeroP, ArrowForward, ArrowRight } from './HeroElements'
-import { Button } from '../ButtonElements'
-import {Link} from "react-router-dom";
+
+import React, { useState, useEffect, useRef } from 'react';
+import Typed from 'typed.js';
+import Video from '../../videos/video1.mp4';
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroBtnWrapper, HeroH1, HeroP, ArrowForward, ArrowRight } from './HeroElements';
+import { Button } from '../ButtonElements';
+import { Link } from 'react-router-dom';
+
 const HeroSection = () => {
-    const [hover, setHover] = useState(false)
+  const [hover, setHover] = useState(false);
+  const el = useRef(null);
 
-    const onHover = () => {
-        setHover(!hover)
-    }
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['Make your life more blissful'],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+      showCursor: false,
+    });
 
-    return (
-        <HeroContainer id='home'>
-            <HeroBg>
-                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
-            </HeroBg>
-            <HeroContent
-                whileInView={{ y: [ 100, 50, 0], opacity: [0, 0, 1]}}
-                transition={{duration: 0.8}}
-            >
-                <HeroH1>Make your life more Blissful</HeroH1>
-                <HeroP>
-                    Sign up now and give your life a second chance
-                </HeroP>
-                <HeroBtnWrapper>
-                    <Button as={Link} to='/signin' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true' style={{ textDecoration: 'none' }}>
-                        Get Started {hover ? <ArrowForward /> : <ArrowRight />}
-                    </Button>
-                </HeroBtnWrapper>
-            </HeroContent>
-        </HeroContainer>
-    )
-}
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
-export default HeroSection
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent
+        whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+        transition={{ duration: 0.8 }}
+      >
+        <HeroH1>
+          <span ref={el} style={{ display: 'inline-block' }} />
+        </HeroH1>
+        <HeroP>
+          Sign up now and give your life a second chance
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            as={Link}
+            to="/signin"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+            style={{ textDecoration: 'none' }}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
--- src/Components/InfoSection/index.js
+++ src/Components/InfoSection/index.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, {  useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 import { Button } from '../ButtonElements';
 import { AboutContainer, AboutWrapper, AboutRow, Column1, Column2, TextWrapper, TopLine, Heading, SubTitle, BtnWrap, ImgWrap, Img } from './InfoElements'
 
 const About = ({ lightBg, id, imgStart, topLine, headline, description, buttonLabel, lightText, darkText, img, alt, primary, dark, dark2 }) => {
-  return (
-    <>
 
 
-      <AboutContainer lightBg={lightBg} id={id} >
 
-        <AboutWrapper>
-          <AboutRow imgStart={imgStart}>
-            <Column1>
-              <TextWrapper
-                whileInView={{ y: [ 100, 50, 0], opacity: [0, 0, 1]}}
-                transition={{duration: 0.8}}
-              >
-                <TopLine>{topLine}</TopLine>
-                <Heading>{headline}</Heading>
-                <SubTitle>{description}</SubTitle>
+const el = useRef(null);
+
+useEffect(() => {
+  const typed = new Typed(el.current, {
+    strings: [headline],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    showCursor: false,
+  });
+
+  return () => {
+    typed.destroy();
+  };
+}, [headline]);
+
+
+return (
+  <>
+
+
+    <AboutContainer lightBg={lightBg} id={id} >
+
+      <AboutWrapper>
+        <AboutRow imgStart={imgStart}>
+          <Column1>
+            <TextWrapper
+              whileInView={{ y: [ 100, 50, 0], opacity: [0, 0, 1]}}
+              transition={{duration: 0.8}}
+            >
+              <TopLine>{topLine}</TopLine>
+              <Heading>   <span ref={el} style={{ display: 'inline-block' }}/></Heading>
+              <SubTitle>{description}</SubTitle>
                 <BtnWrap>
                   <Button to='home'
                     smooth={true}
