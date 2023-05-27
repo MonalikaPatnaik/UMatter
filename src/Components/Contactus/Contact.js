4bcacaa054aa940dacc51eb47bc285244bbbca50
Arun Sharma <98419710+2020B1531074@users.noreply.github.com>
2023-05-28 00:48:20 +0530

    Update Contact.js
    Add a placeholder in contactus section

--- src/Components/Contactus/Contact.js
+++ src/Components/Contactus/Contact.js
@@ -8,19 +8,18 @@ const Contact = () => {
     <form>
       <div class="form-group">
         <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required />
+        <input type="text" id="name" name="name" required placeholder="Enter your name" />
       </div>
       
       <div class="form-group">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required />
-      </div>
-      
-      <div class="form-group">
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
-      </div>
-      
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email" required placeholder="Enter your email" />
+</div>
+
+<div class="form-group">
+  <label for="message">Message:</label>
+  <textarea id="message" name="message" required placeholder="Enter your message"></textarea>
+</div>
       <div class="form-group">
         <button type="submit">Submit</button>
       </div>
