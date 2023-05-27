b6272abec96fb8a82f96db7d53a13d2b0eaa738e
Arun Sharma <98419710+2020B1531074@users.noreply.github.com>
2023-05-27 18:59:06 +0530

    Update Contact.js
    there is a adding a placeholder in contact us section

--- src/Components/Contactus/Contact.js
+++ src/Components/Contactus/Contact.js
@@ -1,33 +1,31 @@
 import React from "react";
 import "./Contact.css";
+
 const Contact = () => {
   return (
-    
-  <div class="form-container">
-    <h2>Contact Us</h2>
-    <form>
-      <div class="form-group">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required />
-      </div>
-      
-      <div class="form-group">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required />
-      </div>
-      
-      <div class="form-group">
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
-      </div>
-      
-      <div class="form-group">
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-  </div>
+    <div className="form-container">
+      <h2>Contact Us</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required placeholder="Enter your name" />
+        </div>
 
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required placeholder="Enter your email" />
+        </div>
 
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" required placeholder="Enter your message"></textarea>
+        </div>
+
+        <div className="form-group">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
