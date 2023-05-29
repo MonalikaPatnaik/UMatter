d9a0ba5fb2c8b8349bb62e3c4b1cbc5c80a8eae2
pooranjoyb <pooranjoyb2016@gmail.com>
2023-05-29 11:10:22 +0530

    Added Hooks for Data Management

--- src/Components/Contactus/Contact.js
+++ src/Components/Contactus/Contact.js
@@ -1,32 +1,60 @@
-import React from "react";
+import { React, useState } from "react";
+
 import "./Contact.css";
+
 const Contact = () => {
-  return (
+  const [name, setName] = useState('');
+  const [mail, setMail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    const mailData = {
+      name,
+      mail,
+      message,
+    };
+
+    await fetch('http://localhost:8081/mail', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(mailData)
+    })
     
-  <div class="form-container">
-    <h2>Contact Us</h2>
-    <form>
-    <div class="form-group">
-  <label for="name">Name:</label>
-  <input type="text" id="name" name="name" required placeholder="Enter your name"/>
-</div>
+    alert("Contact Request Sent Successfully")
+  }
 
-<div class="form-group">
-  <label for="email">Email:</label>
-  <input type="email" id="email" name="email" required placeholder="Enter your email"/>
-</div>
 
-<div class="form-group">
-  <label for="message">Message:</label>
-  <textarea id="message" name="message" placeholder="Enter your message"></textarea>
-</div>
 
-      
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-  </div>
+  return (
+
+    <div class="form-container">
+      <h2>Contact Us</h2>
+      <form onSubmit={handleSubmit}>
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" required placeholder="Enter your name" onChange={(e) => setName(e.target.value)} />
+        </div>
+
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required placeholder="Enter your email" onChange={(e) => setMail(e.target.value)} />
+        </div>
+
+        <div class="form-group">
+          <label for="message">Message:</label>
+          <textarea id="message" name="message" placeholder="Enter your message" onChange={(e) => setMessage(e.target.value)}></textarea>
+        </div>
+
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
 
 
   );
