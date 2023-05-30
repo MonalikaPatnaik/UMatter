4d58a0861d1f46ba3a29ab19f02fa0f28b469e6f
Shivam Sharma <meshivam81@gmail.com>
2023-05-30 21:52:39 +0530

    Revert "changed Pswrd togg button position"

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import './App.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages';
-import SigninPage from './pages/signin';
-import Error404 from './pages/Error404';
-import BlogPage from './pages/blogs';
-import FeedbackPage from './pages/feedback';
-import Forgot_password from './pages/ForgotPassword';
-import SignupPage from './pages/signup';
-import Profile from './pages/profile';
-import Navbar from './Components/Navbar';
-import ContactPage from './pages/contact';
+import React from "react";
+import "./App.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import SigninPage from "./pages/signin";
+import Error404 from "./pages/Error404";
+import BlogPage from "./pages/blogs";
+import FeedbackPage from "./pages/feedback";
+import Forgot_password from "./pages/ForgotPassword";
+import SignupPage from "./pages/signup";
+import Profile from "./pages/profile";
+import Navbar from "./Components/Navbar";
+import ContactPage from "./pages/contact";
 
 // import Contact from "./Components/Contactus/Contact";
 // window.$crisp=[];
 // window.CRISP_WEBSITE_ID="e79efdd2-abee-4a1e-b868-c7929585ebd9";
 
 function App() {
-	return (
-		<HashRouter>
-			<Navbar />
-			<Routes>
-				<Route exact path="/" element={<Home />} />
-				<Route exact path="/signin" element={<SigninPage />} />
-				<Route path="/blogs" element={<BlogPage />} />
-				<Route path="/feedback" element={<FeedbackPage />} />
-				<Route path="/contact" element={<ContactPage />} />
+  return (
+    <HashRouter>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/signin" element={<SigninPage />} />
+        <Route path="/blogs" element={<BlogPage />} />
+        <Route path="/feedback" element={<FeedbackPage />} />
+        <Route path="/contact" element={<ContactPage />} />
 
-				<Route
-					exact
-					path="/signin/forgotPassword"
-					element={<Forgot_password />}
-				/>
-				<Route path="/*" element={<Error404 />} />
-				<Route exact path="/signup" element={<SignupPage />} />
-				<Route exact path="/profile" element={<Profile />} />
-			</Routes>
-		</HashRouter>
-	);
+        <Route
+          exact
+          path="/signin/forgotPassword"
+          element={<Forgot_password />}
+        />
+        <Route path="/*" element={<Error404 />} />
+        <Route exact path="/signup" element={<SignupPage />} />
+        <Route exact path="/profile" element={<Profile />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
