19a51417964e0209cb672f0862c5819861a2a520
Monalika Patnaik <monalikapatnaik114@gmail.com>
2023-06-06 19:13:17 +0530

    errors-resolved

--- src/Components/Signup/index.js
+++ src/Components/Signup/index.js
@@ -112,166 +112,6 @@ const SignUp = () => {
 	;
   }
 
-
-  const sendPostRequest = async (e) => {
-    console.log("sendPostRequest exicuted!!!");
-    const response = await fetch("http://localhost:8081/SignUp", {
-      mode: "cors",
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    console.log(response);
-    const result = await response.json();
-    console.log(result);
-  };
-
-  return (
-    <>
-      <br />
-      <br />
-      <DarkMode />
-
-      <Container>
-        <FormWrap>
-          <FormContent>
-            <Form onSubmit={sendPostRequest} action="#">
-              <FormH1>Create an Account</FormH1>
-
-              <FormInput
-                onChange={(e) => setData({ ...data, name: e.target.value })}
-                id="FullNameInput"
-                placeholder="Enter your Full Name"
-                type="text"
-              ></FormInput>
-              <br />
-
-              <FormInput
-                onChange={(e) => setData({ ...data, username: e.target.value })}
-                id="usernameInput"
-                placeholder="Enter Username"
-                type="text"
-              ></FormInput>
-              <br />
-
-              <FormInput
-                onChange={(e) => setData({ ...data, email: e.target.value })}
-                id="emailInput"
-                placeholder="email@example.com"
-                type="email"
-                require
-              />
-              <br />
-
-              <FormInput
-                onChange={(e) =>
-                  setData({ ...data, contactNumber: e.target.value })
-                }
-                id="ContactNumber"
-                placeholder="Enter phone number"
-                type="number"
-              ></FormInput>
-              <br />
-
-              <div style={{ position: "relative" }}>
-                <FormInput
-                  onChange={(e) =>
-                    setData({ ...data, password1: e.target.value })
-                  }
-                  id="PasswordInput"
-                  placeholder="Enter Password"
-                  type={passwordType}
-                />
-                {passwordType === "password" ? (
-                  <i
-                    className="fa-solid fa-eye-slash"
-                    id="eye"
-                    style={{
-                      position: "absolute",
-                      top: "50%",
-                      right: "10px",
-                      transform: "translateY(-50%)",
-                      cursor: "pointer",
-                    }}
-                    onClick={handleclick}
-                  ></i>
-                ) : (
-                  <i
-                    className="fa-solid fa-eye"
-                    id="eye"
-                    style={{
-                      position: "absolute",
-                      top: "50%",
-                      right: "10px",
-                      transform: "translateY(-50%)",
-                      cursor: "pointer",
-                    }}
-                    onClick={handleclick}
-                  ></i>
-                )}
-              </div>
-              <br />
-
-              <div style={{ position: "relative" }}>
-                <FormInput
-                  onChange={(e) =>
-                    setData({ ...data, password1: e.target.value })
-                  }
-                  id="PasswordInput"
-                  placeholder="Re Enter Password"
-                  type={passwordType}
-                />
-                {passwordType === "password" ? (
-                  <i
-                    className="fa-solid fa-eye-slash"
-                    id="eye"
-                    style={{
-                      position: "absolute",
-                      top: "50%",
-                      right: "10px",
-                      transform: "translateY(-50%)",
-                      cursor: "pointer",
-                    }}
-                    onClick={handleclick}
-                  ></i>
-                ) : (
-                  <i
-                    className="fa-solid fa-eye"
-                    id="eye"
-                    style={{
-                      position: "absolute",
-                      top: "50%",
-                      right: "10px",
-                      transform: "translateY(-50%)",
-                      cursor: "pointer",
-                    }}
-                    onClick={handleclick}
-                  ></i>
-                )}
-              </div>
-
-              <button
-                onClick={handleclick}
-                style={{
-                  width: "fit-content",
-                  position: "absolute",
-                  right: "38rem",
-                  bottom: "20rem",
-                  background: "transparent",
-                  border: "none",
-                }}
-              ></button>
-              <FormButton type="submit">Continue</FormButton>
-              <Text>Forgot password?</Text>
-            </Form>
-          </FormContent>
-        </FormWrap>
-      </Container>
-    </>
-  );
-
 	const sendPostRequest = async (e) => {
 		console.log('sendPostRequest exicuted!!!');
 		const response = await fetch('http://localhost:8081/SignUp', {
@@ -458,10 +298,12 @@ const SignUp = () => {
 								style={{
 									width: 'fit-content',
 									position: 'absolute',
-									right: '38rem',
-									bottom: '20rem',
-									background: 'transparent',
-									border: 'none',
+										right: '0%',
+										top: '26%',
+										background: 'transparent',
+										color: '#10b981',
+										border:'none',
+							
 								}}
 							></button>
 							<FormButton type="submit">Continue</FormButton>
