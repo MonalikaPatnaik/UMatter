aaee6a45224ddb88a0e72b4c6d29c97067db645c
Suchitra Sahoo <123376847+Suchitra-Sahoo@users.noreply.github.com>
2023-06-20 18:18:41 +0530

    Issue #68 Habit Tracker
    Added a Habbit Tracker option under the Services Section. Worked under GSoC'23

--- src/Components/Services/CardAnimation.js
+++ src/Components/Services/CardAnimation.js
@@ -33,3 +33,14 @@ export const cardThreeVariants = {
         transition: {delay: 0.15, duration: 1}
     }
 }
+export const cardFourVariants = {
+    hidden: {
+        opacity: 0,
+        x: 200
+    },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {delay: 0.15, duration: 1}
+    }
+}
