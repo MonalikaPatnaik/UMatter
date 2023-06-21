8761c9e210f24f2ee7d500152112dd8ef5822f78
guha-rahul <52607971+guha-rahul@users.noreply.github.com>
2023-06-22 02:51:14 +0530

    Update index.js

--- src/Components/Blogs/index.js
+++ src/Components/Blogs/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Icon1 from "../../images/no-drinking.png";
 import Icon2 from "../../images/smoking-is-Injurious-to-Health.jpeg";
 import Icon3 from "../../images/Embracing_the_Power_of_Menstruation.png";
@@ -22,7 +22,6 @@ import {
 } from "./BlogsElements";
 
 const Blogs = () => {
-
   const [searchTerm, setSearchTerm] = useState("");
 
   const navigate = useNavigate();
@@ -61,7 +60,7 @@ const Blogs = () => {
             padding: "8px",
             textAlign: "center",
             justifyContent: "center",
-            border : "3px solid black"
+            border: "3px solid black",
 
             // Add any other desired inline styles
           }}
@@ -93,9 +92,15 @@ const Blogs = () => {
               <b>{blog.title}</b>
             </BlogsH1>
             <BlogsP>{blog.content}</BlogsP>
-            <a href={blog.hyperlink}>
-              <ReadMoreButton>Read more</ReadMoreButton>
-            </a>
+            {blog.hyperlink == "" ? (
+              <Link to="./abc">
+                <ReadMoreButton>Read more</ReadMoreButton>
+              </Link>
+            ) : (
+              <a target="_blank" href={blog.hyperlink}>
+                <ReadMoreButton>Read more</ReadMoreButton>
+              </a>
+            )}
           </BlogsCard>
         ))}
       </BlogsWrapper>
@@ -111,73 +116,80 @@ const blogsData = [
     title: "5 Sophisticated Non-Alcoholic Cocktails to Try on your Date Night",
     content:
       "For those looking for a more sober option, alcohol-free cocktails can be the perfect way to give your evening a touch of sophistication.",
-    hyperlink:'https://alcoholchange.org.uk/blog/2023/5-sophisticated-non-alcoholic-cocktails-to-try-on-your-date-night',
+    hyperlink:
+      "https://alcoholchange.org.uk/blog/2023/5-sophisticated-non-alcoholic-cocktails-to-try-on-your-date-night",
   },
   {
     icon: Icon2,
     title: "What Smoking Does to Your Body",
     content:
       "Did you know long-term smoking and vaping can lead to many health issues like high blood pressure, strokes, heart conditions, and various forms of cancer? Smoking and vaping can also cause blockages in your body.",
-    hyperlink:'https://www.healthline.com/health/smoking/effects-on-body',
+    hyperlink: "",
   },
   {
     icon: Icon8,
     title: "Isolation - the secret enemy",
     content:
       "Nature is ideal for lowering stress levels, but it’s also a great way to remind yourself there’s a whole world out there and it can be comforting to see the bigger picture when we step outside our bubbles.",
-     hyperlink:'https://alcoholchange.org.uk/blog/2023/week-five-isolation-the-secret-enemy',
+    hyperlink:
+      "https://alcoholchange.org.uk/blog/2023/week-five-isolation-the-secret-enemy",
   },
   {
     icon: Icon10,
     title: "Learning how to say no",
     content:
       "As well as fear, people can sniff out indecision, so you need to make sure that your rebuttal to why you aren’t drinking is both confident and watertight. It doesn’t need to be lengthy.",
-    hyperlink:'https://www.betterup.com/blog/how-to-say-no',
+    hyperlink: "https://www.betterup.com/blog/how-to-say-no",
   },
   {
     icon: Icon3,
     title: "Embracing the Power of Menstruation",
     content:
       "Menstruation involves destigmatizing it, recognizing its significance, advocating for menstrual health, and challenging societal norms to celebrate the strength and beauty of women's bodies.",
-    hyperlink:'https://www.futureofpersonalhealth.com/womens-health/embracing-the-power-of-the-period/',
+    hyperlink:
+      "https://www.futureofpersonalhealth.com/womens-health/embracing-the-power-of-the-period/",
   },
   {
     icon: Icon5,
     title: "Distraction techniques to avoid cravings",
     content:
       "If you get a craving or experience minor withdrawal, use distraction until the urge passes. Distract your mind and your body. If you get a craving, chew on gum, talk a walk, open a window, or begin a new activity.",
-    hyperlink:'https://edition.cnn.com/2016/06/08/health/food-cravings-mindful-distraction-solution/index.html',
+    hyperlink:
+      "https://edition.cnn.com/2016/06/08/health/food-cravings-mindful-distraction-solution/index.html",
   },
   {
     icon: Icon6,
     title: "Support groups can help you quit smoking",
     content:
       "Let your friends, family, and coworkers know of your plans to stop smoking and your quit date. It helps for people around you to be aware of what you are going through, especially when you are grumpy.",
-    hyperlink:'https://medlineplus.gov/ency/article/007440.htm',
+    hyperlink: "https://medlineplus.gov/ency/article/007440.htm",
   },
   {
     icon: Icon4,
     title: "The 5D's that would help with smoking cessation",
     content:
       "No matter where you are in your journey of quitting, the five Ds are healthy coping mechanisms. The five Ds are: Delay, Distract, Drink water, Deep breathing, Discuss",
-    hyperlink:'https://www.verywellmind.com/memorize-the-5-ds-to-beat-smoking-urges-2825310',
+    hyperlink:
+      "https://www.verywellmind.com/memorize-the-5-ds-to-beat-smoking-urges-2825310",
   },
   {
     icon: Icon9,
     title: "Few natural remedies to cope with nicotine withdrawal",
     content:
       "As a substitute for smoking, try chewing on carrots, pickles, apples, celery, sugarless gum, or hard candy. Keeping your mouth busy may stop the psychological need to smoke.",
-    hyperlink:'https://www.cancer.gov/about-cancer/causes-prevention/risk/tobacco/withdrawal-fact-sheet#:~:text=Remind%20yourself%20that%20cravings%20will,the%20psychological%20need%20to%20smoke.',
+    hyperlink:
+      "https://www.cancer.gov/about-cancer/causes-prevention/risk/tobacco/withdrawal-fact-sheet#:~:text=Remind%20yourself%20that%20cravings%20will,the%20psychological%20need%20to%20smoke.",
   },
   {
     icon: Icon10,
     title: "How does the Medical ChatBot works?",
     content:
       "So, a chatbot is a software that stimulates or makes a human-like conversational environment. Let me tell you that everyone in their life has interacted with chatbots whether one knows or not. As an example let us say you are doing your work on a computer some research on it, and a window pops up that says if you need help. Or while you are going to a good restaurant and ask your google assistant or Siri about which hotel will suit you. So these are scenarios in which you are having an encounter with a chatbot.",
-  }, {
+  },
+  {
     icon: Icon9,
     title: "How to quit smoking according to Ayurveda?",
     content:
       "Consumption of a measured mixture of ashwagandha, Bala, shatavari and ginseng every day, or a chyawanprash containing these help in detoxification. Consume a teaspoon of Ajwain,it reduces cravings of smoke and results in reducing the effect so intake nicotine.",
-  }, 
+  },
 ];
