184a095d91f588f15db47360902374a6539dca1d
Parmesh Bhatt <101285245+Parmesh119@users.noreply.github.com>
2023-06-22 17:11:29 +0530

    Update index.js

--- src/Components/Blogs/index.js
+++ src/Components/Blogs/index.js
@@ -173,7 +173,7 @@ const blogsData = [
   }, 
   {
     icon: Icon9,
-    title: "How meditation improved the Productivity for a Developer",
+    title: "How meditation improved the Productivity for a Developer?",
     content:
       "Once I integrated meditation into my daily routine, I started to see a shift in my productivity at work too. Meditation helped me to be more mindful while working, which made me more aware of when my body needed to take breaks. This turned out to be very helpful in my ability to solve coding problems as I no longer was stuck in tunnel vision. A good break helped me to refocus my attention on other things and the answer to the problem came randomly while washing the dishes or while doing other errands. I am sure you experienced this too. Let me know if did! ",
   }, 
