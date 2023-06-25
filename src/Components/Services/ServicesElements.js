2366aa2b5f92916bc73f360703442454ee7d0df8
karthik <karthiknadar1204@gmail.com>
2023-06-25 16:58:14 +0530

    About page revamp

--- src/Components/Services/ServicesElements.js
+++ src/Components/Services/ServicesElements.js
@@ -1,3 +1,96 @@
+// import styled from "styled-components";
+// import { motion } from "framer-motion";
+
+// export const ServicesContainer = motion(styled.div`
+//   height: 800px;
+//   padding: 30px;
+//   padding-bottom: 150px;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   background: var(--bg-clr);
+
+//   @media screen and (max-width: 768px) {
+//     height: 1400px;
+//   }
+//   @media screen and (max-width: 992px) {
+//     height: 1200px;
+//   }
+//   @media screen and (max-width: 480px) {
+//     height: 1500px;
+//   }
+// `);
+
+// export const ServicesWrapper = styled.div`
+//   max-width: 1000px;
+//   margin: 0 auto;
+//   display: grid;
+//   grid-template-columns: 1fr 1fr 1fr;
+//   align-items: center;
+//   grid-gap: 16px;
+//   padding: 0 30px;
+
+//   @media screen and (max-width: 1000px) {
+//     grid-template-columns: 1fr 1fr;
+//   }
+//   @media screen and (max-width: 768px) {
+//     grid-template-columns: 1fr;
+//     padding: 0 20px;
+//   }
+// `;
+
+// export const ServicesCard = styled.div`
+//   align-items: center;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: flex-start;
+//   border-radius: 10px;
+//   height: 380px;
+//   padding: 30px;
+//   box-shadow: 0 1px 3px gray;
+//   color: var(--text-clr);
+//   transition: all 0.2s ease-in-out;
+
+//   &:hover {
+//     transform: scale(1.02);
+//     transition: all 0.2s ease-in-out;
+//     cursor: pointer;
+//   }
+// `;
+
+// export const ServicesIcon = styled.img`
+//   height: 170px;
+//   width: 180px;
+//   margin-bottom: 10px;
+//   border: 0.5px solid gray;
+//   box-shadow: 3px 3px 3px gray;
+// `;
+
+// export const ServicesH1 = styled.h1`
+//   margin-bottom: 64px;
+//   font-size: 50px;
+//   line-height: 1.1;
+//   font-weight: 600;
+//   text-transform: uppercase;
+//   color: var(--text-clr);
+
+//   @media screen and (max-width: 480px) {
+//     font-size: 2rem;
+//   }
+// `;
+
+// export const ServicesH2 = styled.h2`
+//   font-size: 1rem;
+//   margin-bottom: 10px;
+// `;
+
+// export const ServicesP = styled.p`
+//   font-size: 1rem;
+//   text-align: center;
+// `;
+
+
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
@@ -23,13 +116,20 @@ export const ServicesContainer = motion(styled.div`
 `);
 
 export const ServicesWrapper = styled.div`
-  max-width: 1000px;
-  margin: 0 auto;
+  max-width: 110rem;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   align-items: center;
   grid-gap: 16px;
   padding: 0 30px;
+  background-color:#ffd60a;
+  margin-top:15rem;
+  height:40rem;
+  width:104rem;
+  border-radius:1.2cm 0 0 1.2cm;
+  position:absolute;
+  right:0;
+  
 
   @media screen and (max-width: 1000px) {
     grid-template-columns: 1fr 1fr;
@@ -37,6 +137,7 @@ export const ServicesWrapper = styled.div`
   @media screen and (max-width: 768px) {
     grid-template-columns: 1fr;
     padding: 0 20px;
+    width:44rem;
   }
 `;
 
@@ -49,6 +150,7 @@ export const ServicesCard = styled.div`
   height: 380px;
   padding: 30px;
   box-shadow: 0 1px 3px gray;
+  // background:#293241;
   color: var(--text-clr);
   transition: all 0.2s ease-in-out;
 
@@ -61,14 +163,14 @@ export const ServicesCard = styled.div`
 
 export const ServicesIcon = styled.img`
   height: 170px;
-  width: 180px;
+  width: auto;
   margin-bottom: 10px;
   border: 0.5px solid gray;
   box-shadow: 3px 3px 3px gray;
 `;
 
 export const ServicesH1 = styled.h1`
-  margin-bottom: 64px;
+  margin-bottom: 30rem;
   font-size: 50px;
   line-height: 1.1;
   font-weight: 600;
@@ -87,5 +189,20 @@ export const ServicesH2 = styled.h2`
 
 export const ServicesP = styled.p`
   font-size: 1rem;
-  text-align: center;
+  text-align: center; 
 `;
+ 
+export const CarouselContainer = styled.div`
+  position: relative;
+  right: -29rem;
+  top:12rem;
+  transform: translateY(-50%);
+  width: 90%;
+  @media screen and (max-width: 768px){
+    position:relative;
+    right:1rem;
+    width:65%;
+    margin-left:3rem;
+  }
+`;
+
--- src/Components/Services/index.js
+++ src/Components/Services/index.js
@@ -1,18 +1,138 @@
 
+// import React from 'react';
+// import Icon1 from '../../images/image1.png';
+// import Icon2 from '../../images/image2.png';
+// import Icon3 from '../../images/image3.png';
+// import Icon4 from '../../images/image4.png';
+// import { Services_Data } from '../Services/Services_Data';
+// import { ServicesContainer, ServicesH1, ServicesH2, ServicesWrapper, ServicesCard, ServicesIcon, ServicesP } from './ServicesElements';
+
+
+// import { useEffect, useRef } from "react";
+// import Typed from "typed.js";
+// import { motion } from "framer-motion";
+// import {
+//   cardOneVariants,
+//   cardTwoVariants,
+//   cardThreeVariants,
+//   cardFourVariants,
+// } from "./CardAnimation";
+
+// const Services = () => {
+//   const el = useRef(null);
+
+//   useEffect(() => {
+//     const typed = new Typed(el.current, {
+//       strings: ["Our services"],
+//       typeSpeed: 50,
+//       backSpeed: 50,
+//       loop: true,
+//       showCursor: false,
+//     });
+
+//     return () => {
+//       typed.destroy();
+//     };
+//   }, []);
+
+//   return (
+//     <ServicesContainer id="services">
+//     <ServicesH1>
+//       <span ref={el} style={{ display: "inline-block" }} />
+//     </ServicesH1>
+
+
+//     <ServicesWrapper>
+//       <motion.div
+//         variants={cardOneVariants}
+//         initial="hidden"
+//         animate="visible"
+//       >
+//         <ServicesCard>
+//           <ServicesIcon src={Services_Data[0].icon} />
+//           <ServicesH2>
+//             <b>{Services_Data[0].title}</b>
+//           </ServicesH2>
+//           <ServicesP>
+//             {Services_Data[0].description}
+//           </ServicesP>
+//         </ServicesCard>
+//       </motion.div>
+
+//       <motion.div
+//         variants={cardTwoVariants}
+//         initial="hidden"
+//         animate="visible"
+//       >
+//         <ServicesCard>
+//           <ServicesIcon src={Services_Data[1].icon} />
+//           <ServicesH2>
+//             <b>{Services_Data[1].title}</b>
+//           </ServicesH2>
+//           <ServicesP>
+//             {Services_Data[1].description}
+//           </ServicesP>
+//         </ServicesCard>
+//       </motion.div>
+
+
+//       <motion.div
+//         variants={cardThreeVariants}
+//         initial="hidden"
+//         animate="visible"
+//       >
+//         <ServicesCard>
+//           <ServicesIcon src={Services_Data[2].icon} />
+//           <ServicesH2>
+//             <b>{Services_Data[2].title}</b>
+//           </ServicesH2>
+//           <ServicesP>
+//             {Services_Data[2].description}
+//           </ServicesP>
+//         </ServicesCard>
+//       </motion.div>
+
+
+//       <motion.div
+//         variants={cardFourVariants}
+//         initial="hidden"
+//         animate="visible"
+//       >
+//         <ServicesCard>
+//           <ServicesIcon src={Services_Data[3].icon} />
+//           <ServicesH2>
+//             <b>{Services_Data[3].title}</b>
+//           </ServicesH2>
+//           <ServicesP>
+//             {Services_Data[3].description}
+//           </ServicesP>
+//         </ServicesCard>
+//       </motion.div>
+//     </ServicesWrapper>
+//   </ServicesContainer>
+
+//   );
+// };
+
+// export default Services;
+
+
 import React from 'react';
 import Icon1 from '../../images/image1.png';
 import Icon2 from '../../images/image2.png';
 import Icon3 from '../../images/image3.png';
 import Icon4 from '../../images/image4.png';
 import { Services_Data } from '../Services/Services_Data';
-import { ServicesContainer, ServicesH1, ServicesH2, ServicesWrapper, ServicesCard, ServicesIcon, ServicesP } from './ServicesElements';
+import { ServicesContainer, ServicesH1, ServicesH2, ServicesWrapper, ServicesCard, ServicesIcon, ServicesP,CarouselContainer } from './ServicesElements';
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 import { useEffect, useRef } from "react";
 import Typed from "typed.js";
 import { motion } from "framer-motion";
-import {
-  cardOneVariants,
+import { 
+  cardOneVariants, 
   cardTwoVariants,
   cardThreeVariants,
   cardFourVariants,
@@ -36,18 +156,30 @@ const Services = () => {
   }, []);
 
   return (
+    <>
     <ServicesContainer id="services">
+
     <ServicesH1>
       <span ref={el} style={{ display: "inline-block" }} />
     </ServicesH1>
 
-
     <ServicesWrapper>
+    <CarouselContainer>
+    <Carousel
+          showThumbs={false}
+          showStatus={false}
+          infiniteLoop={true}
+          autoPlay={true}
+          interval={5000} // adjust the interval duration as needed
+          stopOnHover={true}
+          swipeable={true}
+          emulateTouch={true}
+        >
       <motion.div
         variants={cardOneVariants}
         initial="hidden"
         animate="visible"
-      >
+        >
         <ServicesCard>
           <ServicesIcon src={Services_Data[0].icon} />
           <ServicesH2>
@@ -63,7 +195,7 @@ const Services = () => {
         variants={cardTwoVariants}
         initial="hidden"
         animate="visible"
-      >
+        >
         <ServicesCard>
           <ServicesIcon src={Services_Data[1].icon} />
           <ServicesH2>
@@ -75,12 +207,11 @@ const Services = () => {
         </ServicesCard>
       </motion.div>
 
-
       <motion.div
         variants={cardThreeVariants}
         initial="hidden"
         animate="visible"
-      >
+        >
         <ServicesCard>
           <ServicesIcon src={Services_Data[2].icon} />
           <ServicesH2>
@@ -92,12 +223,11 @@ const Services = () => {
         </ServicesCard>
       </motion.div>
 
-
       <motion.div
         variants={cardFourVariants}
         initial="hidden"
         animate="visible"
-      >
+        >
         <ServicesCard>
           <ServicesIcon src={Services_Data[3].icon} />
           <ServicesH2>
@@ -108,10 +238,16 @@ const Services = () => {
           </ServicesP>
         </ServicesCard>
       </motion.div>
+    </Carousel>
+  </CarouselContainer>
     </ServicesWrapper>
   </ServicesContainer>
+        </>
 
   );
 };
 
 export default Services;
+
+
+
