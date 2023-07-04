bc2f88051054896a7d7811c93fb36d2816c1e80b
Himanshu Narware <himanshunarware77@gmail.com>
2023-07-04 17:50:48 +0530

    Feedback page UI improvement done

--- src/Components/Feedback/FeedbackElements.js
+++ src/Components/Feedback/FeedbackElements.js
@@ -31,7 +31,7 @@ export const FormContent = styled.div`
 `;
 
 export const Form = styled.form`
- background: linear-gradient(45deg, black,  rgb(1, 147, 86) , rgb(10, 201, 122));
+background:#fff;
  max-width: 550px;
  height: auto;
  width: 100%;
@@ -40,7 +40,7 @@ export const Form = styled.form`
  margin: 90px auto 40px;
  padding: 40px 32px;
  border-radius: 4px;
- box-shadow: 1px 2px 10px rgba(0, 0, 0, 0.9);
+ box-shadow: 1px 2px 10px #01bf71;
 
  @media screen and (max-width: 400px) {
     padding: 40px 32px;
@@ -49,28 +49,28 @@ export const Form = styled.form`
 
 export const FormH1 = styled.label`
  margin-bottom: 40px;
- color: #fff;
- font-size: 20px;
- font-weight: 400;
+ color: #01bf71;
+ font-size: 30px;
+ font-weight: 600;
  text-align: center;
  `;
 
 export const FormLabel = styled.label`
  margin-bottom: 8px;
- color: #fff;
- font-size: 14px;
+ color: #01bf71;
+ font-size: 1.3rem;
+ font-weight:600;
  cursor:pointer;
-`;
-
-export const FormInput = styled.input`
-margin-bottom: 40px;
-padding: 10px;
+ `;
+ 
+ export const FormInput = styled.input`
+ margin-bottom: 40px;
+ padding: 10px;
 border: none;
-border-radius: 4px;
-font-size:18px;
+font-size:1rem;
 border:none;
 outline:none;
-background:rgb(255,255,255);
+background:#b2ecd4;
 :focus{
    border-bottom:5px solid #01bf71;
    transition:opacity 0.4s ease;
@@ -79,6 +79,12 @@ background:rgb(255,255,255);
 :invalid{
    border-bottom:5px solid red;
 }
+&:hover {
+   border: 1px solid #01bf71;
+ }
+ &::placeholder {
+   color: rgba(0, 0, 0, 0.5);
+ }
 @media screen and (max-width:435px){
    font-size:14px;
 }
@@ -86,12 +92,14 @@ background:rgb(255,255,255);
 
 export const FormButton = styled.button`
  background: #01bf71;
- padding: 16px 10px;
+ padding: 12px 10px;
  border: none;
- border-radius: 12px;
+ border-radius: 10px;
  color: #fff;
  font-size: 20px;
  cursor: pointer;
+ width:12rem;
+ margin-inline:auto;
  &:hover {
    opacity : 0.8;
    transition: opacity 0.25s ease;
