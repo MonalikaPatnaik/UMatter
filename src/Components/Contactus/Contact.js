72f7334bb104928e4583fb5e7ae38258f184c4fb
Geeta <geetkiroula1999@gmail.com>
2023-07-06 15:45:18 +0530

    Validation add on contact form to enhance UI

--- src/Components/Contactus/Contact.js
+++ src/Components/Contactus/Contact.js
@@ -5,27 +5,59 @@ const Contact = () => {
   const [name, setName] = useState('');
   const [mail, setMail] = useState('');
   const [message, setMessage] = useState('');
+  const [error, seterror] = useState('');
+  const [success, setsuccess] = useState('');
+
+  function validEmail(email) {
+    let re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(email)) return true;
+    else return false;
+  }
 
   const handleSubmit = async (e) => {
 
     e.preventDefault();
 
-    const mailData = {
-      name,
-      mail,
-      message,
-    };
+    console.log("called");
 
-    await fetch('http://localhost:8081/mail', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(mailData)
-    })
-    
-    alert("Contact Request Sent Successfully")
- 
+    if(name === '')
+    { seterror("nameerr");}
+  
+    else if(mail === '')
+    { seterror("emailerr");}
+
+    else if(!validEmail(mail))
+    { seterror("validerr");}
+   
+    else if(message === '')
+    {seterror("messageerr");}
+
+    else
+    {
+          const mailData = {
+            name,
+            mail,
+            message,
+          };
+      
+          await fetch('http://localhost:3000/mail', {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(mailData)
+          })
+          
+          setsuccess("Contact Request Sent Successfully!!");
+          seterror('');
+          setName('');
+          setMail('');
+          setMessage('');
+         // alert("Contact Request Sent Successfully")
+   
+    }
+  
   }
 
 
@@ -35,24 +67,51 @@ const Contact = () => {
     <div class="form-container">
       <h2>Contact Us</h2>
       <hr></hr>
+      <div><p className="text-lg font-normal text-green-600 my-2">{success}</p></div>
       <form onSubmit={handleSubmit}>
         <div class="form-group">
           <label for="name">Name:</label>
-          <input type="text" id="name" name="name" required placeholder="Enter your name" onChange={(e) => setName(e.target.value)} />
+          <input type="text" id="name" name="name" required placeholder="Enter your name"   value={name} onChange={(e) => setName(e.target.value)} 
+           className={`${error==='nameerr'?"inputError": name!==''?"successError":""}`}
+          />
+          {
+                error === 'nameerr' && (
+                  <small className='text-red-600 text-lg'>*Name is Required!</small>
+                ) 
+              }
         </div>
 
         <div class="form-group">
           <label for="email">Email:</label>
-          <input type="email" id="email" name="email" required placeholder="Enter your email" onChange={(e) => setMail(e.target.value)} />
+          <input type="email" id="email" name="email" required placeholder="Enter your email"   value={mail} onChange={(e) => setMail(e.target.value)}
+          className={`${error==='emailerr' || error==='validerr'?"inputError": mail!==''?"successError":""}`}
+          />
+          {
+                error === 'emailerr' && (
+                  <small className='text-red-600 text-lg'>*E-mail is Required!</small>
+                ) 
+              }
+              {
+                error === 'validerr' && (
+                  <small className='text-red-600 text-lg'>*Valid E-mail is Required!</small>
+                ) 
+              }
         </div>
 
         <div class="form-group">
           <label for="message">Message:</label>
-          <textarea id="message" name="message" placeholder="Enter your message" onChange={(e) => setMessage(e.target.value)}></textarea>
+          <textarea id="message" name="message" placeholder="Enter your message"  value={message} onChange={(e) => setMessage(e.target.value)}
+          className={`${error==='messageerr'?"inputError": message!==''?"successError":""}`}
+          ></textarea>
+          {
+                error === 'messageerr' && (
+                  <small className='text-red-600 text-lg'>*Message is Required!</small>
+                ) 
+              }
         </div>
 
         <div>
-          <button type="submit">Submit</button>
+          <button onClick={handleSubmit} type="submit">Submit</button>
         </div>
       </form>
     </div>
