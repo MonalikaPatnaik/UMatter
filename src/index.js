6250ffb595e109420931e7ddd6ca8aac2658c15a
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-01-27 21:42:11 +0530

    added-modules

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { ToastContainer } from "react-toastify";
+import { Helmet } from 'react-helmet';
 import "react-toastify/dist/ReactToastify.css";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Helmet>
+        {/* Tawk.to Script */}
+        <script type="text/javascript">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/65b528d50ff6374032c590fc/1hl5qf1dp';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </script>
+      </Helmet>
      <ToastContainer />
     <App />
   </React.StrictMode>
