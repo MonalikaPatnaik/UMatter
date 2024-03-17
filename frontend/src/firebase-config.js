72b58be60c11dde789e1f756820c4896e67beb29
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-03-18 00:34:49 +0530

    authentication

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider} from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: `${process.env.apiKey}`,
-    authDomain: `${process.env.authDomain}`,
-    projectId: `${process.env.projectId}`,
-    storageBucket: `${process.env.storageBucket}`,
-    messagingSenderId: `${process.env.messagingSenderId}`,
-    appId: `${process.env.appId}`,
+    apiKey: `${process.env.REACT_APP_apiKey}`,
+    authDomain: `${process.env.REACT_APP_authDomain}`,
+    projectId: `${process.env.REACT_APP_projectId}`,
+    storageBucket: `${process.env.REACT_APP_storageBucket}`,
+    messagingSenderId: `${process.env.REACT_APP_messagingSenderId}`,
+    appId: `${process.env.REACT_APP_appId}`,
 };
 
 console.log("apiKey:", process.env.REACT_APP_apiKey);
